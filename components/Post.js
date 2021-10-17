e7e14d416b9137065107f653e8fdbfce0034e8cd
AnhHoangQuach <hoanganh36.work@gmail.com>
2021-10-17 13:51:15 +0700

    feat: add comment input and delete line console

--- components/Post.js
+++ components/Post.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import ShareIcon from '@mui/icons-material/Share';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
 
 export default function Post({ id, username, userImg, img, caption }) {
-  console.log(username);
   return (
-    <div>
+    <div className="bg-white my-7 border rounded-sm">
       <div className="flex items-center p-5">
         <img
           src={userImg}
@@ -14,6 +18,32 @@ export default function Post({ id, username, userImg, img, caption }) {
         <p className="flex-1 font-bold">{username}</p>
         <MoreHorizIcon className="h-5" />
       </div>
+
+      <img src={img} alt="" className="w-full object-cover" />
+
+      <div className="flex justify-between px-4 pt-4">
+        <div className="flex space-x-4">
+          <FavoriteBorderIcon className="btn" />
+          <ChatBubbleOutlineIcon className="btn" />
+          <ShareIcon className="btn" />
+        </div>
+        <BookmarkBorderIcon className="btn" />
+      </div>
+
+      <p className="p-5 truncate">
+        <span className="font-bold mr-1">{username}</span>
+        {caption}
+      </p>
+
+      <form className="flex items-center p-4">
+        <SentimentSatisfiedIcon className="h-7" />
+        <input
+          type="text"
+          placeholder="Add a comment..."
+          className="border-none flex-1 focus:ring-0 outline-none"
+        />
+        <button className="font-semibold text-blue-400">Post</button>
+      </form>
     </div>
   );
 }
